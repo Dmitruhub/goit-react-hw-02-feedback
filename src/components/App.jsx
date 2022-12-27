cc88234e91fa93157fa402ad56de193c0d690792
Dmitruhub <dmitrumail@gmail.com>
2022-12-27 17:40:27 +0200

    add GlobalStyle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,5 @@
 import { GlobalStyle } from 'GlobalStyles';
 
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <GlobalStyle />;
 };
